c9ef0be1a48e598802293296ada982c9b98bde33
ddchen <842914439@qq.com>
2015-09-17 14:29:25 +0800

    add json compare equal function

--- src/utils.js
+++ src/utils.js
@@ -10,6 +10,56 @@ function stringify (val) {
     }
 }
 
+var equal = () => {
+    let maxDepth,
+        curDepth,
+        isArray = Array.isArray;
+
+    let isPureObj = (json) => json && typeof json === "object" && !isArray(json);
+    let getLen = (json) => {
+        if(isArray(json)) return json.length;
+        if(json && typeof json === "object") return Object.getOwnPropertyNames(json).length;
+    };
+    let nextPath = (path, next) => path === "" ? next : path + "." + next;
+    let joinRes = (res, json1, json2, path) => res ? { pass: true } : { pass: false, path, json1, json2 };
+
+    let eq = (json1, json2, path = "") => {
+        if(curDepth > maxDepth) {
+            throw new Error("exceed max recursive depth: 7");
+        }
+        let isBothArr = isArray(json1) && isArray(json2);
+        let isBothObj = isPureObj(json1) && isPureObj(json2);
+
+        let json = getLen(json1) > getLen(json2) ? json1 : json2;
+        if (isBothArr) {
+            curDepth++;
+            for (let i = 0; i < json.length; i++) {
+                let eqRes = eq(json1[i], json2[i], nextPath(path, i));
+                if (!eqRes.pass) {
+                    return eqRes;
+                }
+            }
+        } else if (isBothObj) {
+            curDepth++;
+            for (let name in json) {
+                let eqRes = eq(json1[name], json2[name], nextPath(path, name));
+                if (!eqRes.pass) {
+                    return eqRes;
+                }
+            }
+        } else if (json1 !== json2) {
+            return joinRes(false, json1, json2, path);
+        }
+
+        return joinRes(true);
+    };
+    return (json1, json2, path, md) => {
+        curDepth = 1;
+        maxDepth = md || 7;
+        return eq(json1, json2, path);
+    };
+}();
+
 export default {
     extend: (obj, src) => {
         for (var key in src) {
@@ -19,10 +69,7 @@ export default {
     },
 
     eq: (formatAssertErr) => (actual, expected) => {
-        actual = stringify(actual);
-        expected = stringify(expected);
-
-        if (actual === expected)
+        if(equal(actual, expected).pass)
             return Promise.resolve();
 
         let { stack } = new Error("Assertion");
@@ -34,7 +81,7 @@ export default {
             );
 
         return Promise.reject(
-            formatAssertErr(actual, expected, stack)
+            formatAssertErr(stringify(actual), stringify(expected), stack)
         );
     }
 };
