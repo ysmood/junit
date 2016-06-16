6b8800425f660ef859a9dde51eb6bf0a68c2fd32
Yad Smood <y.s.inside@gmail.com>
2016-06-16 16:40:49 +0800

    fix: error stack bug

--- test/lab/index.js
+++ test/lab/index.js
@@ -1,36 +1,12 @@
 var junit = require("../../lib");
+// var kit = require('nokit');
 
-var it = junit();
-var eq = it.eq;
+var it = junit({
+});
 
 it.describe("level 01", function (it) {
     it("test 01", function () {
-        return eq(1, 1);
-    });
-
-    it("test 02", function () {
-        return eq(1, 1);
-    });
-
-    it.describe("level 02", function (it) {
-
-        it.describe("level 03", function (it) {
-            it("test 01", function () {
-                return eq(1, 1);
-            });
-
-            it("test 02", function () {
-                return eq(1, 1);
-            });
-        });
-
-        it("test 01", function () {
-            return eq(1, 1);
-        });
-
-        it("test 02", function () {
-            return eq(1, 1);
-        });
+        return it.eq(1, 3);
     });
 });
 
